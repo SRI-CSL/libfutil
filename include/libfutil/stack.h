4c5fc369a2764097298c4c386b524e99f2605b26
Jeroen Massar <massar@fsi.io>
2013-09-16 16:46:17 +0000

    Remove superflous line

--- include/libfutil/stack.h
+++ include/libfutil/stack.h
@@ -1,7 +1,6 @@
 #ifndef STACK_H
 #define STACK_H 1
 
-
 void dump_stacktrace(void **trace, uint64_t *trace_size, unsigned int skip);
 void format_stacktrace(char *buf, unsigned int length, void **trace, unsigned int trace_size);
 void output_stacktrace(void);
